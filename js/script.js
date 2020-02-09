199d238ecd1c12788ce04fed4a8d8f3aafd2d6bb
Jacob Yoon <jacobyoon926@gmail.com>
2020-02-09 17:54:19 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,52 +1,16 @@
-window.addEventListener('mousemove', cursor);
-var $win = $(window);
-var _cursor = document.getElementById('cursor');
-var _cursorFollower = document.getElementById('cursorFollower');
-
-function everyTick() {
-    cursor();
-    setTimeout(arguments.callee, 0);
-}
-
-function getX(event) //left position
-{
-    if(!event.pageX)
-    {
-        return event.clientX;
-    }
-    else
-    {
-        return event.pageX - (document.body.scrollLeft || document.documentElement.scrollLeft);
-    }
-}
-
-function getY(event) //top position
-{
-    if(event.pageY)
-    {
-        return event.pageY - (document.body.scrollTop || document.documentElement.scrollTop);
-    }
-    else
-    {
-        return event.clientY;
-    }
-}
-
 var offset = 50;
 var halfWidth = window.innerWidth / 2;
 var halfHeight = window.innerHeight / 2;
 
-function cursor() {
+document.addEventListener("mousemove", e => {
     if (window.innerWidth > 1000) {
-        var midX = (getX(event) - halfWidth) * -.8;
-        var midY = (getY(event) - halfHeight) * -.8;
-        _cursor.style.top = getY(event) - 20 + "px";
-        _cursor.style.left = getX(event) - 20 + "px";
-        _cursorFollower.style.top = getY(event) - offset + "px";
-        _cursorFollower.style.left = getX(event) - offset + "px";
+        document.getElementById('cursor').style.top = e.pageY - 20 + "px";
+        document.getElementById('cursor').style.left = e.pageX - 20 + "px";
+        var midX = (e.pageY - halfWidth) * -.8;
+        var midY = (e.pageY - halfHeight) * -.8;
         $('#projects').css('transform', 'translateX(' + midX + 'px) translateY(' + midY + 'px)');
     }
-}
+})
 
 $win.on('scroll', function(){
     console.log(window.innerWidth);
